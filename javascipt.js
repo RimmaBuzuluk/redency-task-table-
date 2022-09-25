8dfb9f862ea9409180eb900685394d190d75c6ae
RimmaBuzuluk <rimmysya2000@gmail.com>
2022-09-25 14:13:07 +0300

    new

--- javascipt.js
+++ javascipt.js
@@ -39,7 +39,7 @@ const generateElem = (name, created, categor, contest, dates) => {
         <th class="created">${created}</th>
         <th class="category">${categor}</th>
         <th class="content">${contest}</th>
-        <th сlass="data_">${dates}</th>
+        <th сlass="datam">${dates}</th>
         <th class="btn">
             <button class="btn_arh" ><img src="https://cdn-icons-png.flaticon.com/512/1732/1732309.png" alt=""></button>
             <button class="btn_del"><img src="https://cdn-icons-png.flaticon.com/512/70/70757.png" alt=""></button>
@@ -58,6 +58,7 @@ tbody.innerHTML = elemTable;
 
 let tbody_ar = document.getElementById("id_arh");
 
+/////////////////////////////////УДАЛЕНИЕ ЭЛЕМЕНТА ИЗ ГЛАВНОГО МАССИВА
 //коли кнопка нажата-видаляти карточку із структури DOM
 //1.Обрати всі кнопки
 
@@ -68,15 +69,14 @@ function handlClick(e) {
   const currentButton = e.currentTarget;
   currentButton.closest(".new_table").remove();
   //console.log(currentButton.parentElement);
+  console.log(elemList);
 }
 //3.використовувати цикл щоб додати обробник події до кожної кнопки
 buttons_del.forEach((buttons_del) => {
   buttons_del.addEventListener("click", handlClick);
 });
 
-// /////////////////архив
-
-///////////////////////
+// ///////////////// СОЗДАНИЕ АРХТВА
 
 ///////create element in table///////
 const tbody_arh = document.createElement("tbody");
@@ -88,13 +88,13 @@ let table = document.getElementById("table_arh");
 table.appendChild(tbody_arh);
 
 const elemListArhiv = [
-  {
-    name: "ssss",
-    created: 2015,
-    categor: "slss",
-    contest: "sslsl",
-    dates: "s,sks",
-  },
+  // {
+  //   name: "ssss",
+  //   created: 2015,
+  //   categor: "slss",
+  //   contest: "sslsl",
+  //   dates: "s,sks",
+  // },
 ];
 
 const generateElemArhiv = (name, created, categor, contest, dates) => {
@@ -123,55 +123,52 @@ const elemTableArhiv = elemListArhiv
   .join("");
 tbody_arh.innerHTML = elemTableArhiv;
 
-////////////////////////////////////////////////
+/////////////////////////ДОБАВЛЕНИЕ НОВОГО ЭЛЕМЕНТА
 function newElementCreate() {
-  const tr_new = document.createElement("tr");
-  tr_new.classList.add("newElement");
-  let tbody_new = document.getElementById("tbody");
-  tbody_new.appendChild(tr_new);
-
-  const elemListNew = [
-    {
-      name: "new",
-      created: "nwe",
-      categor: "new",
-      contest: "nwe",
-      dates: "new",
-    },
-  ];
+  const elemListNew = {
+    name: "new",
+    created: "nwe",
+    categor: "new",
+    contest: "nwe",
+    dates: "new",
+  };
+  elemList.push(elemListNew);
+  console.log(elemList);
 
   const generateElemNew = (name, created, categor, contest, dates) => {
     return `
-    <tr class="new_element">
-        <th>${name}</th>
-        <th>${created}</th>
-        <th>${categor}</th>
-        <th>${contest}</th>
-        <th>${dates}</th>
+    <tr class="new_table">
+        <th class="name">${name}</th>
+        <th class="created">${created}</th>
+        <th class="category">${categor}</th>
+        <th class="content">${contest}</th>
+        <th сlass="datam">${dates}</th>
         <th class="btn">
             <button class="btn_arh" ><img src="https://cdn-icons-png.flaticon.com/512/1732/1732309.png" alt=""></button>
             <button class="btn_del"><img src="https://cdn-icons-png.flaticon.com/512/70/70757.png" alt=""></button>
             <button class="btn_red"><img src="https://w7.pngwing.com/pngs/672/1022/png-transparent-writing-computer-icons-blog-write-miscellaneous-angle-text.png"></button>
         </th>
-
-     </tr>
+     </tr>   
     `;
   };
 
-  const elemTableNew = elemListNew
-    .map((el_new) => {
+  const elemTable = elemList
+    .map((el) => {
       return generateElemNew(
-        el_new.name,
-        el_new.created,
-        el_new.categor,
-        el_new.contest,
-        el_new.dates
+        el.name,
+        el.created,
+        el.categor,
+        el.contest,
+        el.dates
       );
     })
     .join("");
-  tr_new.innerHTML = elemTableNew;
+  tbody.innerHTML = elemTable;
+
+  // let tbody_ar = document.getElementById("id_arh");
 }
 
+////////////////ДОБАВЛЕНИЕ НОВОГО ЭЛЕМЕНА В АРХИВ
 //коли кнопка нажата-видаляти карточку із структури main та додати в arhiv
 //1.Обрати всі кнопки архіву
 
@@ -179,7 +176,7 @@ const buttons_arh = document.querySelectorAll(".btn_arh");
 // console.log(buttons_arh);
 
 //2.Створити функцію архівації
-function handlClick(e) {
+function handlClickArhiv(e) {
   const currentButton = e.currentTarget;
   //видылити з тега объект
   const div_elem = currentButton.closest(".new_table");
@@ -196,17 +193,23 @@ function handlClick(e) {
   const categor =
     document.getElementsByClassName("category")[string_index - 1].innerHTML;
   console.log(categor);
-  const content =
+  const contest =
     document.getElementsByClassName("content")[string_index - 1].innerHTML;
-  console.log(content);
-  const data =
-    document.getElementsByClassName("data_")[string_index - 1].innerHTML;
-  console.log(data);
+  console.log(contest);
+  // const dates =
+  //   document.getElementsByClassName("datam")[string_index - 1].innerHTML;
+  // console.log(dates);
 
   ///////////////////////////////////////////////////
-
+  let new_elem = {
+    name: name,
+    created: created,
+    categor: categor,
+    contest: contest,
+    dates: "ddff",
+  };
   //запушити элемент  в нову таблицю
-  // elemListArhiv.push(elemForPush);
+  elemListArhiv.push(new_elem);
   //видалити едеменнт зі старої таблиці
   currentButton.closest(".new_table").remove();
   //обновити  таблицю
@@ -240,5 +243,5 @@ function handlClick(e) {
 
 //3.використовувати цикл щоб додати обробник події до кожної кнопки
 buttons_arh.forEach((buttons_arh) => {
-  buttons_arh.addEventListener("click", handlClick);
+  buttons_arh.addEventListener("click", handlClickArhiv);
 });
