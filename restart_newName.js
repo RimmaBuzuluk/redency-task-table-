78fa9b884c0dad1ecb4222b620a0eb93a0799dfa
RimmaBuzuluk <rimmysya2000@gmail.com>
2022-11-07 13:10:32 +0200

    data

--- restart_newName.js
+++ restart_newName.js
@@ -1,12 +1,28 @@
 function restart_newName(string_index) {
   //////////////работа с новыми и старыми значениями
   ////получаем номер строки которую изменяем
-  n = string_index;
-  console.log(string_index);
   console.log(
     "название новое ",
-    document.getElementsByClassName("name")[n - 1].innerHTML
+    document.getElementsByClassName("name")[string_index - 1].innerHTML
   );
   number();
-  console.log("старое название", elemList[n - 1].name);
+  console.log("старое название", elemList[string_index - 1].name);
+
+  elemList[string_index - 1].name =
+    document.getElementsByClassName("name")[string_index - 1].innerHTML;
+  elemList[string_index - 1].created =
+    document.getElementsByClassName("created")[string_index - 1].innerHTML;
+  elemList[string_index - 1].categor =
+    document.getElementsByClassName("category")[string_index - 1].innerHTML;
+  elemList[string_index - 1].contest =
+    document.getElementsByClassName("content")[string_index - 1].innerHTML;
+  //   elemList[string_index - 1].dates =
+  //     document.getElementsByClassName("datam")[string_index - 1].innerHTML;
+
+  console.log(
+    "название новое ",
+    document.getElementsByClassName("name")[string_index - 1].innerHTML
+  );
+  number();
+  console.log("старое название", elemList[string_index - 1].name);
 }
